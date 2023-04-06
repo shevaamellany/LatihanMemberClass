3164ed0a1afa9b12ec2be832df6db3aea14fa21a
ShevaMellany <128024052+shevaamellany@users.noreply.github.com>
2023-04-06 14:46:04 +0700

    Add files via upload

---
+++ PlanetTicket.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LatihanMemberClass
+{
+    class PlanetTicket
+    {
+        public string Origin { get; set;  }
+        public string Destination { get; set; }
+        public int Cost { get; set;}
+        public string Currency { get; set; }
+
+        public void PrintSummary()
+        {
+            Console.WriteLine("Origin: {0}", Origin);
+            Console.WriteLine("Destination: {0}", Destination);
+            Console.WriteLine("Cost: {0} ({1})", Cost, Currency);
+            if (Currency == "IDR")
+            {
+                Cost = Cost / 15000;
+                Console.WriteLine("Convert to Dolar: {0}", Cost);
+            }
+            else if (Currency == "USD")
+            {
+                Cost = Cost * 15000;
+                Console.WriteLine("Convert to Rupiah: {0}", Cost);
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LatihanMemberClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PlanetTicket ticket1 = new PlanetTicket();
+            PlanetTicket ticket2 = new PlanetTicket();
+
+            ticket1.Origin = "JOGJA";
+            ticket1.Destination = "PKU";
+            ticket1.Cost = 100;
+            ticket1.Currency = "USD";
+
+            ticket2.Origin = "JKT";
+            ticket2.Destination = "PKU";
+            ticket2.Cost = 750000;
+            ticket2.Currency = "IDR";
+
+            ticket1.PrintSummary();
+            Console.WriteLine();
+            ticket2.PrintSummary();
+
+            Console.ReadKey();
+              
+        }
+    }
+}
